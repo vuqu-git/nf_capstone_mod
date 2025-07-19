55776d7e1cf718cb60b5ce14aed9965e7abe4374
vuqu-git <vuqu@fantasymail.de>
2025-07-20 00:29:39 +0200

    *changed ReminderService NOT to use get request a screening endpoint with rest client

--- backend/src/main/java/org/pupille/backend/reminder/ReminderService.java
+++ backend/src/main/java/org/pupille/backend/reminder/ReminderService.java
@@ -2,6 +2,7 @@ package org.pupille.backend.reminder;
 
 import org.pupille.backend.contact.ContactService;
 import org.pupille.backend.mysql.screening.FilmDTOMailReminder;
+import org.pupille.backend.mysql.screening.ScreeningService;
 import org.pupille.backend.mysql.screening.TerminDTOWithFilmDTOMailReminder;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -13,32 +14,114 @@ import java.util.List;
 @Service
 public class ReminderService {
 
-    private final RestClient restClient;
+//    private final RestClient restClient;
+//    private final ContactService contactService;
+//    private final ScreeningService screeningService;
+//
+//    public ReminderService(RestClient.Builder restClient, ContactService contactService, ScreeningService screeningService) {
+//        this.restClient = restClient
+//                .baseUrl("http://localhost:8080") // <--- Add your API's base URL here
+//                .build();
+//
+//        this.contactService = contactService;
+//        this.screeningService = screeningService;
+//    }
+
+//    @Scheduled(cron = "45 23 1 * * *", zone = "Europe/Berlin")
+//    public void scheduledReminder() {
+//
+//        final int FUTURE_DAYS = 10;
+//        List<TerminDTOWithFilmDTOMailReminder> futureScreenings = restClient.get()
+//                .uri("/api/screenings/future/{days}", FUTURE_DAYS)
+//                .retrieve()
+//                .body(new ParameterizedTypeReference<>() {});
+//
+//        if (futureScreenings != null && !futureScreenings.isEmpty()) {
+//                                                      // .isEmpty() returns true if the list contains no elements,  returns false if the list contains one or more elements.
+//            for (TerminDTOWithFilmDTOMailReminder screening : futureScreenings) {
+//
+//                // check if an email is present in patenschaft field in Termin object
+//                if (screening.patenschaft() == null || screening.patenschaft().trim().isEmpty()) {
+//                    //preparation of input parameters for contactService.sendReminder, which sends the mail
+//                    String titel = screening.titel();
+//
+//                    // if it's not a programmtermin, but a Standardtermin (i.e. 1 main feature + optional short film(s)) take the film titel of the 1st feature
+//                    if (titel == null || titel.trim().isEmpty()) {
+//                        List<FilmDTOMailReminder> mainfilms = screening.mainfilms();
+//                        if (mainfilms != null && !mainfilms.isEmpty()) {
+//                            titel = mainfilms.get(0).titel();
+//                        }
+//                    }
+//
+//                    contactService.sendReminder(
+//                            FUTURE_DAYS,
+//                            screening.vorstellungsbeginn().toString(),
+//                            titel,
+//                            "quy8vuong@gmail.com",
+//                            true
+//                    );
+//                }
+//            }
+//        }
+//
+//        // ######################################################
+//
+//        final int PAST_DAYS = 7;
+//        List<TerminDTOWithFilmDTOMailReminder> pastScreenings = restClient.get()
+//                .uri("/api/screenings/past/{days}", PAST_DAYS)
+//                .retrieve()
+//                .body(new ParameterizedTypeReference<>() {});
+//
+//        if (pastScreenings != null && !pastScreenings.isEmpty()) {
+//                                                  // .isEmpty() returns true if the list contains no elements,  returns false if the list contains one or more elements.
+//            for (TerminDTOWithFilmDTOMailReminder screening : pastScreenings) {
+//
+//                // check if an email is present in patenschaft field in Termin object
+//                if (screening.patenschaft() == null || screening.patenschaft().trim().isEmpty()) {
+//                    //preparation of input parameters for contactService.sendReminder, which sends the mail
+//                    String titel = screening.titel();
+//
+//                    // if it's not a programmtermin, but a Standardtermin (i.e. 1 main feature + optional short film(s)) take the film titel of the 1st feature
+//                    if (titel == null || titel.trim().isEmpty()) {
+//                        List<FilmDTOMailReminder> mainfilms = screening.mainfilms();
+//                        if (mainfilms != null && !mainfilms.isEmpty()) {
+//                            titel = mainfilms.get(0).titel();
+//                        }
+//                    }
+//
+//                    contactService.sendReminder(
+//                            PAST_DAYS,
+//                            screening.vorstellungsbeginn().toString(),
+//                            titel,
+//                            "quy8vuong@gmail.com",
+//                            false
+//                    );
+//                }
+//            }
+//        }
+//    }
+
     private final ContactService contactService;
+    private final ScreeningService screeningService;
 
-    public ReminderService(RestClient.Builder restClient, ContactService contactService) {
-        this.restClient = restClient
-                .baseUrl("http://localhost:8080") // <--- Add your API's base URL here
-                .build();
-
+    public ReminderService(ContactService contactService, ScreeningService screeningService) {
         this.contactService = contactService;
+        this.screeningService = screeningService;
     }
 
     @Scheduled(cron = "45 23 1 * * *", zone = "Europe/Berlin")
-    public void scheduledReminder() {
+    public void periodicFetchAndEmail() {
 
         final int FUTURE_DAYS = 10;
-        List<TerminDTOWithFilmDTOMailReminder> futureScreenings = restClient.get()
-                .uri("/api/screenings/future/{days}", FUTURE_DAYS)
-                .retrieve()
-                .body(new ParameterizedTypeReference<>() {});
+
+        List<TerminDTOWithFilmDTOMailReminder> futureScreenings = screeningService.getTermineDaysInFuture(FUTURE_DAYS);
 
         if (futureScreenings != null && !futureScreenings.isEmpty()) {
-                                                      // .isEmpty() returns true if the list contains no elements,  returns false if the list contains one or more elements.
+            // .isEmpty() returns true if the list contains no elements,  returns false if the list contains one or more elements.
             for (TerminDTOWithFilmDTOMailReminder screening : futureScreenings) {
 
                 // check if an email is present in patenschaft field in Termin object
-                if (screening.patenschaft() == null || screening.patenschaft().trim().isEmpty()) {
+                if (screening.patenschaft() != null && !screening.patenschaft().trim().isEmpty()) {
                     //preparation of input parameters for contactService.sendReminder, which sends the mail
                     String titel = screening.titel();
 
@@ -54,9 +137,11 @@ public class ReminderService {
                             FUTURE_DAYS,
                             screening.vorstellungsbeginn().toString(),
                             titel,
-                            "quy8vuong@gmail.com",
+                            screening.patenschaft(),
                             true
                     );
+
+                    System.out.println("Mail (future reminder) sent to " + screening.patenschaft());
                 }
             }
         }
@@ -64,17 +149,15 @@ public class ReminderService {
         // ######################################################
 
         final int PAST_DAYS = 7;
-        List<TerminDTOWithFilmDTOMailReminder> pastScreenings = restClient.get()
-                .uri("/api/screenings/past/{days}", PAST_DAYS)
-                .retrieve()
-                .body(new ParameterizedTypeReference<>() {});
+
+        List<TerminDTOWithFilmDTOMailReminder> pastScreenings = screeningService.getTermineDaysInPast(PAST_DAYS);
 
         if (pastScreenings != null && !pastScreenings.isEmpty()) {
-                                                  // .isEmpty() returns true if the list contains no elements,  returns false if the list contains one or more elements.
+            // .isEmpty() returns true if the list contains no elements,  returns false if the list contains one or more elements.
             for (TerminDTOWithFilmDTOMailReminder screening : pastScreenings) {
 
                 // check if an email is present in patenschaft field in Termin object
-                if (screening.patenschaft() == null || screening.patenschaft().trim().isEmpty()) {
+                if (screening.patenschaft() != null && !screening.patenschaft().trim().isEmpty()) {
                     //preparation of input parameters for contactService.sendReminder, which sends the mail
                     String titel = screening.titel();
 
@@ -90,9 +173,11 @@ public class ReminderService {
                             PAST_DAYS,
                             screening.vorstellungsbeginn().toString(),
                             titel,
-                            "quy8vuong@gmail.com",
+                            screening.patenschaft(),
                             false
                     );
+
+                    System.out.println("Mail (past reminder) sent to " + screening.patenschaft());
                 }
             }
         }
